65a4eeed7ad583e9bb6fc94f468bb7651f86dfa0
Mindohyeon <min50875@naver.com>
2022-11-05 18:09:27 +0900

    :lipstick: Add inputIdTextField

--- Sources/Present/Scene/Auth/Login/ViewController/SignInViewController.swift
+++ Sources/Present/Scene/Auth/Login/ViewController/SignInViewController.swift
@@ -9,6 +9,29 @@ import UIKit
 
 class SignInViewController: BaseVC<SignInViewModel> {
     private let logoImageView = UIImageView().then {
+        $0.contentMode = .scaleAspectFit
         $0.image = FindMeAsset.Images.projectLogoImage.image
+    }
+    
+    private let inputIdTextField = UnderLineTextField().then {
+        $0.setPlaceholder(placeholder: "아이디를 입력해주세요.")
+    }
+    
+    override func addView() {
+        view.addSubViews(logoImageView, inputIdTextField)
+    }
+    
+    override func setLayout() {
+        logoImageView.snp.makeConstraints {
+            $0.top.equalToSuperview().inset(135)
+            $0.leading.trailing.equalToSuperview().inset(95)
+            $0.width.equalTo(148)
+            $0.height.equalTo(49)
+        }
+        
+        inputIdTextField.snp.makeConstraints {
+            $0.top.equalTo(logoImageView.snp.bottom).offset(48)
+            $0.leading.trailing.equalToSuperview().inset(30)
+        }
     }
 }
