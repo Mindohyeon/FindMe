1e21849d04bb12fdb487558c397daeaa460c4b4f
Mindohyeon <min50875@naver.com>
2022-11-05 17:51:48 +0900

    :hammer: Assets Image 접근 용이성 늘리기

--- Sources/Present/Scene/Auth/Login/ViewController/SignInViewController.swift
+++ Sources/Present/Scene/Auth/Login/ViewController/SignInViewController.swift
@@ -5,9 +5,10 @@
 //  Created by 민도현 on 2022/11/04.
 //  Copyright © 2022 com.dohyeon. All rights reserved.
 //
-
-import Foundation
+import UIKit
 
 class SignInViewController: BaseVC<SignInViewModel> {
-
+    private let logoImageView = UIImageView().then {
+        $0.image = FindMeAsset.Images.projectLogoImage.image
+    }
 }
