ce873f9a6ecc1c77427b0f28e873e203cb089bbd
Mindohyeon <min50875@naver.com>
2022-11-09 23:37:32 +0900

    :lipstick: Add MainVC, VM

---
+++ Sources/Present/Scene/Main/MainViewController/MainViewController.swift
@@ -0,0 +1,13 @@
+//
+//  MainViewController.swift
+//  FindMe
+//
+//  Created by 민도현 on 2022/11/09.
+//  Copyright © 2022 com.dohyeon. All rights reserved.
+//
+
+import Foundation
+
+class MainViewController: BaseVC<MainViewModel> {
+    
+}
