dc36a1b6997e6b62e133d6e7399a086c008ae336
Mindohyeon <min50875@naver.com>
2022-11-12 22:07:48 +0900

    :lipstick: Add Tabman 요소 배열 추가

--- Sources/Present/Scene/Main/Home/HomeViewController/HomeViewController.swift
+++ Sources/Present/Scene/Main/Home/HomeViewController/HomeViewController.swift
@@ -6,9 +6,12 @@
 //  Copyright © 2022 com.dohyeon. All rights reserved.
 //
 
-import Foundation
+import UIKit
+import Tabman
 
 class HomeViewController: BaseVC<HomeViewModel> {
+    private let viewControllers = ["전체", "전자기기", "귀금속", "의류", "생활용품", "기타"]
+    
     override func configureVC() {
         navigationController?.navigationBar.prefersLargeTitles = true
         navigationItem.title = "찾고있는 물건들"
