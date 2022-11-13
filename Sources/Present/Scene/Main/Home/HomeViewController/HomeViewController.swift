43a22a2155239a24cba6e55343e777773c5de3fe
Mindohyeon <min50875@naver.com>
2022-11-14 08:23:40 +0900

    :lipstick: Add profileButton 배치

--- Sources/Present/Scene/Main/Home/HomeViewController/HomeViewController.swift
+++ Sources/Present/Scene/Main/Home/HomeViewController/HomeViewController.swift
@@ -12,8 +12,23 @@ import Tabman
 class HomeViewController: BaseVC<HomeViewModel> {
     private let viewControllers = ["전체", "전자기기", "귀금속", "의류", "생활용품", "기타"]
     
+    private let profileButton = UIButton().then {
+        $0.setImage(UIImage(named: FindMeAsset.Images.profileIcon.name), for: .normal)
+    }
+    
     override func configureVC() {
         navigationController?.navigationBar.prefersLargeTitles = true
         navigationItem.title = "찾고있는 물건들"
+    }
+    
+    override func addView() {
+        view.addSubViews(profileButton)
+    }
+    
+    override func setLayout() {
+        profileButton.snp.makeConstraints {
+            $0.top.equalTo(view.safeAreaLayoutGuide).inset(-45)
+            $0.trailing.equalToSuperview().inset(12)
+        }
     }
 }
