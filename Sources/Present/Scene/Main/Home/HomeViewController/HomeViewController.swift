5da11dbd10f019f370f20476d11aaba69ef582e3
Mindohyeon <min50875@naver.com>
2022-11-16 20:18:29 +0900

    :poop: sorry

--- Sources/Present/Scene/Main/Home/HomeViewController/HomeViewController.swift
+++ Sources/Present/Scene/Main/Home/HomeViewController/HomeViewController.swift
@@ -49,26 +49,38 @@ class HomeViewController: TabmanViewController {
     
     
     func settingTabBar (ctBar : TMBar.ButtonBar) {
+//        ctBar.layout.transitionStyle = .snap
+//        // 왼쪽 여백주기
+//        ctBar.layout.contentInset = UIEdgeInsets(top: 0.0, left: 13.0, bottom: 0.0, right: 20.0)
+//        // 간격
+//        ctBar.layout.interButtonSpacing = 35
+//
+//        ctBar.backgroundView.style = .blur(style: .light)
+//        // 선택 / 안선택 색 + font size
+//        ctBar.buttons.customize { (button) in
+//            button.tintColor = .black
+//            button.selectedTintColor = .black
+//            button.font = UIFont.systemFont(ofSize: 16)
+//            button.selectedFont = UIFont.systemFont(ofSize: 16, weight: .medium)
+//        }
+//
+//        // 인디케이터 (영상에서 주황색 아래 바 부분)
+//        ctBar.indicator.weight = .custom(value: 2)
+//        ctBar.indicator.tintColor = .orange
         ctBar.layout.transitionStyle = .snap
-        // 왼쪽 여백주기
-        ctBar.layout.contentInset = UIEdgeInsets(top: 0.0, left: 13.0, bottom: 0.0, right: 20.0)
-        
-        // 간격
-        ctBar.layout.interButtonSpacing = 35
-            
-        ctBar.backgroundView.style = .blur(style: .light)
-        
-        // 선택 / 안선택 색 + font size
-        ctBar.buttons.customize { (button) in
-            button.tintColor = .black
-            button.selectedTintColor = .black
-            button.font = UIFont.systemFont(ofSize: 16)
-            button.selectedFont = UIFont.systemFont(ofSize: 16, weight: .medium)
+        ctBar.layout.interButtonSpacing = 50
+        ctBar.layout.contentInset = UIEdgeInsets(top: 50, left: 0, bottom: -40, right: 0)
+        ctBar.buttons.customize {
+            $0.font = UIFont.systemFont(ofSize: 17)
+            $0.tintColor = .init(red: 0.29, green: 0.26, blue: 0.27, alpha: 1)
+            $0.selectedFont = UIFont.systemFont(ofSize: 17)
+            $0.selectedTintColor = .black
         }
-        
-        // 인디케이터 (영상에서 주황색 아래 바 부분)
-        ctBar.indicator.weight = .custom(value: 2)
-        ctBar.indicator.tintColor = .orange
+        ctBar.indicator.weight = .custom(value: 4)
+        ctBar.indicator.tintColor = .init(red: 0.94, green: 0.47, blue: 0.62, alpha: 1)
+        ctBar.indicator.cornerStyle = .rounded
+        ctBar.layout.alignment = .centerDistributed
+        ctBar.systemBar().backgroundStyle = .clear
     }
 }
 
