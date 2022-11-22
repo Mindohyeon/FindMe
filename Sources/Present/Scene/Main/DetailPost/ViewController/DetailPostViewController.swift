aebcb9d0095f1289efd791703a3d15f7e2459164
Mindohyeon <min50875@naver.com>
2022-11-22 11:16:08 +0900

    :lipstick: Add postDescriptionLabel

--- Sources/Present/Scene/Main/DetailPost/ViewController/DetailPostViewController.swift
+++ Sources/Present/Scene/Main/DetailPost/ViewController/DetailPostViewController.swift
@@ -14,10 +14,16 @@ class DetailPostViewController: BaseVC<DetailPostViewModel> {
     }
     
     private let postTitle = UILabel().then {
+        $0.text = "title"
         $0.font = .boldSystemFont(ofSize: 20)
     }
     
     private let postLocation = UILabel().then {
+        $0.text = "location"
+    }
+    
+    private let postDescriptionLabel = UILabel().then {
+        $0.text = "description"
         $0.textColor = FindMeAsset.Colors.postBodyTextColor.color
     }
     
@@ -26,6 +32,20 @@ class DetailPostViewController: BaseVC<DetailPostViewModel> {
     }
     
     override func setLayout() {
-        <#code#>
+        postImageView.snp.makeConstraints {
+            $0.top.equalTo(view.safeAreaLayoutGuide)
+            $0.leading.trailing.equalToSuperview()
+            $0.size.equalTo(360)
+        }
+        
+        postTitle.snp.makeConstraints {
+            $0.top.equalTo(postImageView.snp.bottom).offset(24)
+            $0.leading.equalToSuperview().inset(12)
+        }
+        
+        postLocation.snp.makeConstraints {
+            $0.top.equalTo(postTitle.snp.bottom).offset(4)
+            $0.leading.equalTo(postTitle.snp.leading).offset(2)
+        }
     }
 }
