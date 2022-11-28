ab575a22ae83b04354514026b161e4ec27cdd24b
Mindohyeon <min50875@naver.com>
2022-11-29 01:54:15 +0900

    :lipstick: Add MapView

--- Sources/Present/Scene/Main/Map/ViewController/MapViewController.swift
+++ Sources/Present/Scene/Main/Map/ViewController/MapViewController.swift
@@ -6,7 +6,131 @@
 //  Copyright © 2022 com.dohyeon. All rights reserved.
 //
 
-import Foundation
+import UIKit
+import MapKit
+import RxSwift
+import RxCocoa
 
-class MapViewController: BaseVC<MapViewModel> {
+class MapViewController: BaseVC<MapViewModel>, findAllItemsPresentable, MKMapViewDelegate, CLLocationManagerDelegate{
+    var findAllItemsData = PublishSubject<[HomeModel]>()
+    
+    let mapView = MKMapView()
+    var locationManager = CLLocationManager()
+    
+    let disposeBag = DisposeBag()
+    
+    func goSetting() {
+        
+        let alert = UIAlertController(title: "위치권한 요청", message: "러닝 거리 기록을 위해 항상 위치 권한이 필요합니다.", preferredStyle: .alert)
+        let settingAction = UIAlertAction(title: "설정", style: .default) { action in
+            guard let url = URL(string: UIApplication.openSettingsURLString) else { return }
+            // 열 수 있는 url 이라면, 이동
+            if UIApplication.shared.canOpenURL(url) {
+                UIApplication.shared.open(url)
+            }
+        }
+        let cancelAction = UIAlertAction(title: "취소", style: .cancel) { UIAlertAction in
+            
+        }
+        
+        alert.addAction(settingAction)
+        alert.addAction(cancelAction)
+        
+        present(alert, animated: true, completion: nil)
+    }
+    
+    func checkCurrentLocationAuthorization(authorizationStatus: CLAuthorizationStatus) {
+        switch authorizationStatus {
+        case .notDetermined:
+            locationManager.requestWhenInUseAuthorization()
+            locationManager.startUpdatingLocation()
+        case .restricted:
+            print("restricted")
+            goSetting()
+        case .denied:
+            print("denided")
+            goSetting()
+        case .authorizedAlways:
+            print("always")
+        case .authorizedWhenInUse:
+            print("wheninuse")
+            locationManager.startUpdatingLocation()
+        @unknown default:
+            print("unknown")
+        }
+        if #available(iOS 14.0, *) {
+            let accuracyState = locationManager.accuracyAuthorization
+            switch accuracyState {
+            case .fullAccuracy:
+                print("full")
+            case .reducedAccuracy:
+                print("reduced")
+            @unknown default:
+                print("Unknown")
+            }
+        }
+    }
+    
+    func checkUserLocationServicesAuthorization() {
+        let authorizationStatus: CLAuthorizationStatus
+        if #available(iOS 14, *) {
+            authorizationStatus = locationManager.authorizationStatus
+        } else {
+            authorizationStatus = CLLocationManager.authorizationStatus()
+        }
+        
+        DispatchQueue.global().async {
+            if CLLocationManager.locationServicesEnabled() {
+                self.checkCurrentLocationAuthorization(authorizationStatus: authorizationStatus)
+            }
+        }
+    }
+    
+    func goLocation(latitudeValue: CLLocationDegrees,
+                    longtudeValue: CLLocationDegrees,
+                    delta span: Double) -> CLLocationCoordinate2D {
+        let pLocation = CLLocationCoordinate2DMake(latitudeValue, longtudeValue)
+        let spanValue = MKCoordinateSpan(latitudeDelta: span, longitudeDelta: span)
+        let pRegion = MKCoordinateRegion(center: pLocation, span: spanValue)
+        mapView.setRegion(pRegion, animated: true)
+        return pLocation
+    }
+    
+    // 특정 위도와 경도에 핀 설치하고 핀에 타이틀과 서브 타이틀의 문자열 표시
+    func setAnnotation(latitudeValue: CLLocationDegrees,
+                       longitudeValue: CLLocationDegrees,
+                       delta span :Double,
+                       title strTitle: String,
+                       subtitle strSubTitle:String){
+        let annotation = MKPointAnnotation()
+        annotation.coordinate = goLocation(latitudeValue: latitudeValue, longtudeValue: longitudeValue, delta: span)
+        annotation.title = strTitle
+        annotation.subtitle = strSubTitle
+        mapView.addAnnotation(annotation)
+    }
+    
+    override func configureVC() {
+        locationManager.requestAlwaysAuthorization()
+        checkUserLocationServicesAuthorization()
+        
+        locationManager.desiredAccuracy = kCLLocationAccuracyBest
+        
+        locationManager.startUpdatingLocation()
+        mapView.showsUserLocation = true
+        
+        viewModel.findAllItems()
+        findAllItemsData.subscribe(onNext: { ee in
+            print("ee = \(ee)")
+        }).disposed(by: disposeBag)
+    }
+    
+    override func addView() {
+        view.addSubViews(mapView)
+    }
+    
+    override func setLayout() {
+        mapView.snp.makeConstraints {
+            $0.top.leading.trailing.bottom.equalTo(view.safeAreaLayoutGuide)
+        }
+    }
 }
--- Sources/Present/Scene/Main/Map/ViewModel/MapViewModel.swift
+++ Sources/Present/Scene/Main/Map/ViewModel/MapViewModel.swift
@@ -7,7 +7,34 @@
 //
 
 import Foundation
+import Alamofire
+import RxSwift
+import RxCocoa
+
+protocol findAllItemsPresentable: AnyObject {
+    var findAllItemsData: PublishSubject<[HomeModel]> { get }
+}
 
 class MapViewModel: BaseViewModel {
+    weak var delegate: findAllItemsPresentable?
     
+    func findAllItems() {
+        let url = APIConstants.findAllPost
+        let headers: HTTPHeaders = ["Content-Type": "application/json", "Accept": "application/json", "Authorization": UserManager.shared.accessToken!]
+        AF.request(url,
+                   method: .get,
+                   encoding: URLEncoding.queryString,
+                   headers: headers)
+        .responseJSON { [weak self] response in
+            let decodeResponse = try? JSONDecoder().decode([HomeModel].self, from: response.data!)
+            self?.delegate?.findAllItemsData.onNext(decodeResponse ?? .init())
+            
+            switch response.result {
+            case .success:
+                print("success")
+            case .failure(let error):
+                print("failure = \(error)")
+            }
+        }
+    }
 }
